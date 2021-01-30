ac591afa0be178c63d81ba95dc482cd997a66c4a
anaaf <anaaf5555@gmail.com>
2021-01-30 09:43:08 +0500

    added testCasesDriver and tesDriver

--- testing.py
+++ testing.py
@@ -1,22 +1,151 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
-# from chromedriver_py import binary_path 
+from LoginTesting import LoginTesting
+from DashboardTesting import DashboardTesting
+from dataDriven import DataDriven
+import time
+import uuid 
 
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(ChromeDriverManager().install())
+dd = DataDriven()
+report = dd.reportDF()
 
-driver.get("https://adactinhotelapp.com/")
+lt = LoginTesting("https://www.linkedin.com/home")
+dt = DashboardTesting(report)
+driver = lt.getDriver()
 
-nameField = driver.find_element_by_id('username')
-nameField.send_keys('Anaaf')
-passField = driver.find_element_by_id('password')
-passField.send_keys('abba109')
-click = driver.find_element_by_id('login').click()
+# rows to record each testCases result
+reportRows = []
+
+#testCases from external file (data driven)
+testCases = dd.data("./data.csv")
 
 
-driver.find_element_by_xpath("//a[normalize-space()='New User Register Here']").click()
-# ('login_register')[0].click()
+def testCaseDriver(testCaseID, testCase, driver):
+
+        if testCaseID == 'TC1':
+            try:
+               reportRows.append({'TestCaseID': testCaseID, 'Result': "", 'Status': ""})
+               reportRows.append(lt.login(lt.getDriver(), testCase[0], testCase[1]))
+               reportRows.append({'TestCaseID': "", 'Result': "", 'Status': "passed"})
+            except:
+                reportRows.append({'TestCaseID': "", 'Result': "", 'Status': "failed"})
+
+        elif testCaseID == 'TC7':
+            reportRows.append({'TestCaseID': testCaseID, 'Result': "", 'Status': ""})
+            reportRows.append(lt.login(lt.getDriver(), testCase[0], testCase[1]))
+            reportRows.append(dt.messageSendingTest(lt.getDriver(), testCase[2], "Hello Ali this is a automated msg"))
+            reportRows.append({'TestCaseID': "", 'Result': "", 'Status': "passed"})
+
+        elif testCaseID == 'TC2':
+            reportRows.append({'TestCaseID': testCaseID, 'Result': "", 'Status': ""})
+            reportRows.append(lt.login(lt.getDriver(), testCase[0], testCase[1]))
+            reportRows.append(lt.logout(lt.getDriver()))
+            reportRows.append({'TestCaseID': "", 'Result': "", 'Status': "passed"})
+        
+        elif testCaseID == 'TC5':
+            reportRows.append({'TestCaseID': testCaseID, 'Result': "", 'Status': ""})
+            reportRows.append(lt.login(lt.getDriver(), testCase[0], testCase[1]))
+            reportRows.append(dt.viewProfileTest(lt.getDriver()))
+            reportRows.append({'TestCaseID': "", 'Result': "", 'Status': "passed"})
+
+        elif testCaseID == 'TC3':
+            reportRows.append({'TestCaseID': testCaseID, 'Result': "", 'Status': ""})
+            reportRows.append(lt.wrongInput(lt.getDriver(), testCase[0], testCase[1], 'email'))
+            reportRows.append({'TestCaseID': "", 'Result': "", 'Status': "passed"})
+        
+        elif testCaseID == 'TC4':
+            reportRows.append({'TestCaseID': testCaseID, 'Result': "", 'Status': ""})
+            reportRows.append(lt.wrongInput(lt.getDriver(), testCase[0], testCase[1], 'password'))
+            reportRows.append({'TestCaseID': "", 'Result': "", 'Status': "passed"})
+        
+        elif testCaseID == 'TC6':
+            reportRows.append({'TestCaseID': testCaseID, 'Result': "", 'Status': ""})
+            reportRows.append(lt.login(lt.getDriver(), testCase[0], testCase[1]))
+            time.sleep(2)
+            reportRows.append( dt.postingTest(lt.getDriver(), testCase[2]))
+            reportRows.append({'TestCaseID': "", 'Result': "", 'Status': "passed"})
+
+
+def testDriver(testCases, rows, testCaseID):
+    for index in range(rows):
+            lt.driverInitializer()
+            lt.driverGet()
+            time.sleep(2)
+            print(testCases[index][3])
+            testCaseDriver(testCases[index][3], testCases[index], driver)
+            lt.driverQuit()
+            time.sleep(4)
+    dd.reportGen(report, reportRows)
+     
+
+
+testDriver(testCases, len(userData), 'TC7')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
